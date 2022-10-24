1a5d46e697b0f14610d87275e80a173fa1ec4fef
Jose Neyra <jose-neyra@outlook.com>
2022-10-24 10:39:30 -0400

    Initial Project Setup

---
+++ src/main/java/com/joseneyra/sfmsbeerservice/SfmsbeerserviceApplication.java
@@ -0,0 +1,13 @@
+package com.joseneyra.sfmsbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfmsbeerserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfmsbeerserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joseneyra/sfmsbeerservice/SfmsbeerserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseneyra.sfmsbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfmsbeerserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
