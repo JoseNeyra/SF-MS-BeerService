6b44403348aafe3aa48bd0bf97918d84709e0417
Jose Neyra <jose-neyra@outlook.com>
2023-06-22 09:23:35 -0400

    beer mapper modifications

--- src/main/java/com/joseneyra/sfmsbeerservice/web/mappers/BeerMapper.java
+++ src/main/java/com/joseneyra/sfmsbeerservice/web/mappers/BeerMapper.java
@@ -11,11 +11,8 @@ public interface BeerMapper {
     @Mapping(target = "quantityOnHand", ignore = true)
     BeerDto beerToBeerDto (Beer beer);
 
-    @Mapping(target = "quantityOnHand", source = "")
     BeerDto beerToBeerDtoWithInventory(Beer beer);
 
-    @Mapping(target = "quantityToBrew", source = "")
-    @Mapping(target = "minOnHand", source = "")
     @Mapping(target = "version", ignore = true)             // Use this when you want to ignore a specific property from being mapped
     Beer beerDtoToBeer (BeerDto dto);                       // In this case the version property of Beer doesnt get mapped to the BeerDto
 }
