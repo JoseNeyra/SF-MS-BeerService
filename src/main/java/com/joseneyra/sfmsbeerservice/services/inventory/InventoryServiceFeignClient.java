201ae49a08c9746660b01886139674cde6db9c2f
Jose Neyra <jose-neyra@outlook.com>
2023-06-23 17:50:28 -0400

    Security modifications

--- src/main/java/com/joseneyra/sfmsbeerservice/services/inventory/InventoryServiceFeignClient.java
+++ src/main/java/com/joseneyra/sfmsbeerservice/services/inventory/InventoryServiceFeignClient.java
@@ -1,6 +1,7 @@
 package com.joseneyra.sfmsbeerservice.services.inventory;
 
 import com.joseneyra.brewery.model.BeerInventoryDto;
+import com.joseneyra.sfmsbeerservice.config.FeignClientConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,7 +11,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.List;
 import java.util.UUID;
 
-@FeignClient(name = "inventory-service", fallback = InventoryServiceFeignClientFailover.class)
+@FeignClient(
+        name = "inventory-service",
+        fallback = InventoryServiceFeignClientFailover.class,
+        configuration = FeignClientConfig.class)        // Bring in the authentication for the server
 public interface InventoryServiceFeignClient {
 
     @RequestMapping(method = RequestMethod.GET, value = BeerInventoryServiceRestTemplateImpl.INVENTORY_PATH)
